d32a253f3f17a961e539ffbb1846a2508c05f8ac
David Vaquero <pepesan@gmail.com>
2022-07-11 14:19:25 +0200

    Ejemplo de uso de varios RequestMapping en una misma clase

--- src/main/java/com/cursosdedesarrollo/demoapp/controllers/MiCrudController.java
+++ src/main/java/com/cursosdedesarrollo/demoapp/controllers/MiCrudController.java
@@ -11,12 +11,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-public class MiController {
-    @RequestMapping("/")
-    public String welcome() {//Welcome page, non-rest
-        return "Welcome to RestTemplate Example.";
-    }
-    @GetMapping("/dato/")
+@RequestMapping("/api/dato")
+public class MiCrudController {
+
+    // Get / Listado
+    @GetMapping("/")
     // @RequestMapping(value = "/dato/",method = RequestMethod.GET)
     public List<Dato> showDato(HttpServletRequest request,
                                HttpServletResponse response)
@@ -30,14 +29,16 @@ public class MiController {
         }
         return listado;
     }
-    @PostMapping("/dato/")
+    // Post Body(dato) / Create
+    @PostMapping("/")
     // @RequestMapping(value = "/dato/",method = RequestMethod.POST)
     public Dato addDato(@RequestBody Dato dato)
             throws ServletException, IOException {
         dato.setId(1L);
         return dato;
     }
-    @GetMapping("/dato/{id}")
+    // Get /{id} - Read
+    @GetMapping("/{id}")
     public Dato showDatoById(@PathVariable Long id)
             throws ServletException, IOException {
         Dato dato= new Dato();
@@ -45,8 +46,10 @@ public class MiController {
         dato.setCadena("Mi cadena "+id);
         return dato;
     }
-
-    @PostMapping("/dato/{id}")
+    // Post  /{id} Modifica todo
+    // Put   /{id} Modifica todo
+    // Patch /{id} Modifica parcialmente
+    @PostMapping("/{id}")
     public Dato editDatoById(
             @PathVariable Long id,
             @RequestBody Dato dato)
@@ -58,7 +61,9 @@ public class MiController {
         //devolver el objeto
         return dato;
     }
-    @DeleteMapping("/dato/{id}")
+    // Get /{id}/delete Borrado
+    // Delete /{id} Borrado
+    @DeleteMapping("/{id}")
     public Dato deleteDatoById(@PathVariable Long id)
             throws ServletException, IOException {
         Dato dato= new Dato();
@@ -67,7 +72,7 @@ public class MiController {
         return dato;
     }
 
-    @GetMapping("/dato/{id}/{otro}")
+    @GetMapping("/{id}/{otro}")
     public Dato showDatoByIdAndByOtro(@PathVariable("id") Long id,@PathVariable("otro") String otro )
             throws ServletException, IOException {
         Dato dato= new Dato();
@@ -76,4 +81,18 @@ public class MiController {
         return dato;
     }
 
+    // Get /{page}/{num} Buscar Paginado
+    @GetMapping("/{page}/{num}")
+    // @RequestMapping(value = "/dato/",method = RequestMethod.GET)
+    public List<Dato> searchDato(@PathVariable("page") Long page, @PathVariable("num") Integer num)
+            throws ServletException, IOException {
+        List<Dato> listado= new ArrayList<Dato>();
+        for (int i = 0; i < 10; i++) {
+            Dato dato= new Dato();
+            dato.setId(new Long(i));
+            dato.setCadena("Mi cadena "+i);
+            listado.add(dato);
+        }
+        return listado;
+    }
 }
